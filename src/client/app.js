5ac312f4d05a8bd92d8240953bea9d71be1c2bb6
Thomas Faller <thomasfaller@Thomass-MacBook-Pro-2.local>
2018-05-17 22:40:40 +0100

    final commit

--- src/client/app.js
+++ src/client/app.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 export default class App extends Component {
   render(){
-    return <div>Welcome to React Boilerplate</div>
+    return <div>Welcome to React Boilerplate App</div>
   }
 }
 
